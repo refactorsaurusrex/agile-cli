1945b53f4a061bace0672a4958c654fc5bc67b01
Nick Spreitzer <nick@200-ok.com>
2020-09-22 00:09:47 -0500

    Enable opening jira issues from cli

---
+++ src/AgileCli/Cmdlets/OpenJiraIssueCmdlet.cs
@@ -0,0 +1,20 @@
+﻿using System.Management.Automation;
+using AgileCli.Services;
+using JetBrains.Annotations;
+
+namespace AgileCli.Cmdlets
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.Open, "JiraIssue")]
+    public class OpenJiraIssueCmdlet : CmdletBase
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true, Position = 0)]
+        public string Key { get; set; }
+
+        protected override void Run()
+        {
+            var hostname = AgileCliConfigurationManager.Load().JiraHostName;
+            SystemProcessFactory.Create().Start($"https://{hostname}/browse/{Key}");
+        }
+    }
+}
